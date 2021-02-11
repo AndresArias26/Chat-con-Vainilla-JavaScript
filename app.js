ec6f1aec264e344a24eda8043a19dd15b073d4ce
AndresArias26 <75452639+AndresArias26@users.noreply.github.com>
2021-02-10 23:09:01 -0500

    Arreglo Errores

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-console.log('Hola desde consola');
+const botones = document.querySelector('#botones')
+const nombreUsuario = document.querySelector('#nombreUsuario')
+
+firebase.auth().onAuthStateChanged (user => {
+    if (user){
+        console.log(user)
+        botones.innerHTML = /*html*/`
+            <button class="btn btn-outline-danger" id='btnCerrarSesion'>Cerrar sesión</button>
+        `
+        nombreUsuario.innerHTML= user.displayName
+        cerrarSesion()
+    }else{
+        console.log('no existe user')
+        botones.innerHTML = /*html*/ `
+            <button class="btn btn-outline-success mr-2" id= 'btnAcceder'>Acceder</button>
+        `
+        iniciarSesion()
+        nombreUsuario.innerHTML= 'Chat'
+    }
+})
+
+const cerrarSesion = () => {
+    const btnCerrarSesion = document.querySelector('#btnCerrarSesion')
+    btnCerrarSesion.addEventListener('click', ()=>{
+        firebase.auth().signOut()
+    })
+}
+
+const iniciarSesion = () =>{
+    const btnAcceder = document.querySelector('#btnAcceder')
+    btnAcceder.addEventListener('click', async() =>{
+        //console.log('me diste click en acceder')
+        try {
+            const provider = new firebase.auth.GoogleAuthProvider()
+            await firebase.auth().signInWithPopup(provider)
+        } catch (error) {
+            console.log(error)
+        }
+    })
+}
